cdda464d608643bb6716331c69ac125e3dfd77ff
Sima <sima.boreiri@gmail.com>
2020-09-09 16:48:29 +0200

    last commit

--- viaplay-challenge/src/App.jsx
+++ viaplay-challenge/src/App.jsx
@@ -1,18 +1,27 @@
-import React, {useState, useEffect} from 'react'
-import axios from 'axios'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const App = () => {
-  const [series, setSeries] = useState([])
+  const [series, setSeries] = useState([]);
 
+  useEffect(() => {
+    getSeriesList();
+  }, []);
 
+  const getSeriesList = async () => {
+    let response = await axios.get(
+      "https://cors-anywhere.herokuapp.com/https://content.viaplay.se/pc-se/serier/samtliga"
+    );
+    setSeries(
+      response.yourDataObject._embedded["viaplay:blocks"][0]._embedded[
+        "viaplay:products"
+      ]
+    );
+  };
 
+  let seriesList = 
 
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  return <div></div>;
+};
 
-export default App
-
+export default App;
